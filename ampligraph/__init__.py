4780ae99984579a165fc4ff38eb31189dd70d3db
Guillermo Tafoya <tafoyamilo@gmail.com>
2023-10-22 00:17:44 -0400

    Update __init__.py
    Commented for compatibility

--- ampligraph/__init__.py
+++ ampligraph/__init__.py
@@ -16,7 +16,9 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 __version__ = '2.0.1'
 __all__ = ['datasets', 'latent_features', 'discovery', 'evaluation', 'utils']
 
-logging.config.fileConfig(
-    pkg_resources.resource_filename(__name__, "logger.conf"),
-    disable_existing_loggers=False,
-)
+### COMMENTED OUT FOR CLOUD COMPATIBILITY ###
+
+#logging.config.fileConfig(
+#    pkg_resources.resource_filename(__name__, "logger.conf"),
+#    disable_existing_loggers=False,
+#)
